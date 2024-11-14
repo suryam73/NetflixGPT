a68a87b7afbc63c37a724957a5f0073f3e1ffa1f
suryam73 <suryamanjula73@gmail.com>
2024-11-14 19:04:39 +0530

    Login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -94,102 +94,47 @@ const Login = () => {
     </div>
 
 
-            <form onClick={(e)=>e.preventDefault()} className='w-[22%] flex flex-col gap-3  h-[75%]  bg-opacity-80 rounded-lg bg-black absolute mx-auto right-0 left-0 my-32 p-16'>
-                <h1 className='text-white text-4xl font-bold mb-6'>
-                    {isSignFrom ? "Sign In " : "Sign Up"}
+    <form onClick={(e) => e.preventDefault()} className="relative  w-full   p-6 bg-black bg-opacity-80 right-0 left-[35%] top-52 rounded-lg sm:w-3/4 lg:w-1/3">
+                <h1 className="text-white text-2xl md:text-4xl font-bold mb-6">
+                    {isSignFrom ? "Sign In" : "Sign Up"}
                 </h1>
 
-                {showError && (                     
-                  <div className="flex py-5 px-3 rounded-md  space-x-2 bg-yellow-300 ">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      role="img"
-                      viewBox="0 0 16 16"
-                      width="20"
-                      height="16"
-                      aria-hidden="true"
-                      className="text-red-600 mt-1"
-                    >
-          
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.46967 5.53033L6.93934 8L4.46967 10.4697L5.53033 11.5303L8 9.06066L10.4697 11.5303L11.5303 10.4697L9.06066 8L11.5303 5.53033L10.4697 4.46967L8 6.93934L5.53033 4.46967L4.46967 5.53033Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    <p className="text-red-600 text-sm">
-                   {showError}
-                    </p>
-                  </div> )}
+                {showError && (
+                    <div className="flex items-center py-2 px-3 mb-4 bg-yellow-300 rounded-md">
+                        <svg className="text-red-600 w-5 h-5 mr-2" viewBox="0 0 16 16" fill="currentColor">
+                            <path fillRule="evenodd" clipRule="evenodd" d="M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.46967 5.53033L6.93934 8L4.46967 10.4697L5.53033 11.5303L8 9.06066L10.4697 11.5303L11.5303 10.4697L9.06066 8L11.5303 5.53033L10.4697 4.46967L8 6.93934L5.53033 4.46967L4.46967 5.53033Z" />
+                        </svg>
+                        <p className="text-red-600 text-sm">{showError}</p>
+                    </div>
+                )}
 
                 {!isSignFrom && (
-                     <input type='text' onChange={(e)=>setName(e.target.value)} placeholder='Full Name' className='p-4 bg-black border  border-slate-400 rounded bg-opacity-65 text-white  w-full'/>
+                    <input type="text" onChange={(e) => setName(e.target.value)} placeholder="Full Name" className="p-3 mb-3 bg-black border border-slate-400 rounded bg-opacity-65 text-white w-full" />
+                )}
+
+                <input type="text" placeholder="Email or mobile number" onChange={(e) => setEmail(e.target.value)} className="p-3 mb-3 bg-black border border-slate-400 rounded bg-opacity-65 text-white w-full" />
+                
+                <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="p-3 mb-3 bg-black border border-slate-400 rounded bg-opacity-65 text-white w-full" />
+
+                <button className="bg-red-600 w-full text-white font-bold rounded h-10 mt-3" onClick={handleCheckValidating}>
+                    {isSignFrom ? "Sign In" : "Sign Up"}
+                </button>
+
+                <span className="text-white text-center my-3 block">OR</span>
+
+                <button className="bg-gray-300 bg-opacity-30 w-full h-10 font-semibold text-white rounded">Use a sign-in code</button>
+                
+                {isSignFrom && (
+                    <span className="text-white text-center mt-3 block">Forgot password?</span>
                 )}
                 
-                <input type='text' placeholder='Email or mobile number' onChange={(e)=>setEmail(e.target.value)} className='p-4 bg-black border border-slate-400 rounded bg-opacity-65 text-white  w-full'/>
-                {email === '' && (
-                  <div className="flex  space-x-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      role="img"
-                      viewBox="0 0 16 16"
-                      width="20"
-                      height="16"
-                      aria-hidden="true"
-                      className="text-red-600 mt-1"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.46967 5.53033L6.93934 8L4.46967 10.4697L5.53033 11.5303L8 9.06066L10.4697 11.5303L11.5303 10.4697L9.06066 8L11.5303 5.53033L10.4697 4.46967L8 6.93934L5.53033 4.46967L4.46967 5.53033Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    <p className="text-red-600 text-sm">
-                      Please enter a valid email address or phone number.
-                    </p>
-                  </div>
-                )}
-                <input type='password' placeholder='Password'  onChange={(e)=>setPassword(e.target.value)}  className='p-4 w-full bg-black border text-white border-slate-400 rounded bg-opacity-65'/>
-                {password  === '' && 
-                (
-                    <div className="flex  space-x-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      role="img"
-                      viewBox="0 0 16 16"
-                      width="20"
-                      height="16"
-                      aria-hidden="true"
-                      className="text-red-600 mt-1"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.46967 5.53033L6.93934 8L4.46967 10.4697L5.53033 11.5303L8 9.06066L10.4697 11.5303L11.5303 10.4697L9.06066 8L11.5303 5.53033L10.4697 4.46967L8 6.93934L5.53033 4.46967L4.46967 5.53033Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    <p className="text-red-600 text-sm">
-                    Your password must contain between 4 and 60 characters.
-                    </p>
-                  </div>
-                )}
-                <button className='bg-red-600 w-full text-white font-bold rounded h-10  ' onClick={handleCheckValidating}>
-                {isSignFrom ? "Sign In " : "Sign Up"}    
-                </button>
-                <span className='text-white text-center'>OR</span>
-                <button className='bg-gray-300 bg-opacity-30 w-full h-10 font-semibold  text-white   rounded '>Use a sign-in code</button>
-                {isSignFrom && (
-                <span  className='text-white text-center'>Forgot password?</span>
-                )}
-                <p className='text-white text-start text-base cursor-pointer' onClick={handelSignFrom} >New to Netflix? Sign up now.</p>
+                <p className="text-white text-center text-base cursor-pointer mt-4" onClick={handelSignFrom}>
+                    {isSignFrom ? "New to Netflix? Sign up now." : "Already have an account? Sign in"}
+                </p>
 
-                 <span  className='text-slate-400 text-start text-sm '>This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.</span>
+                <span className="text-slate-400 text-center text-sm mt-4 block">
+                    This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+                </span>
             </form>
  
     </div>
